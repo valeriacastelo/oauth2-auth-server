ec0e01a0e193d334390a34ace81133ca985fa608
valeriacastelobranco <valeriacastelobranco@gmail.com>
2020-04-10 14:33:11 +0100

    initial commit

---
+++ src/main/java/com/example/oauth2authserver/configuration/JwkAuthServerConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.oauth2authserver.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class JwkAuthServerConfiguration {
+
+}
